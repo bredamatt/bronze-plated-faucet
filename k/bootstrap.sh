c8a9d54953d43a4040c39843b5e0043790fe8c3b
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2022-11-01 14:27:36 +0100

    Delete k

--- k/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-COMMIT_ID=$1
-if [ -z "$COMMIT_ID" ]; then
-    echo "Usage: ./bootstrap.sh <commit-id>"
-    exit 1
-fi
-docker run -it runtimeverificationinc/kframework-k:ubuntu-focal-$COMMIT_ID
