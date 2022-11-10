6792db52262f7c6391670554a0caf20882a13b03
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2022-11-10 13:52:01 +0000

    Add ruster helm chart

--- local-k8s/helmfiles/charts/start-chartmuseum.sh
+++ local-k8s/helmfiles/charts/start-chartmuseum.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-docker run --rm -it \
+docker run --rm \
   -p 8080:8080 \
   -e DEBUG=1 \
   -e STORAGE=local \
   -e STORAGE_LOCAL_ROOTDIR=/charts \
+  -e DISABLE_API=false \
   -v $(pwd)/charts:/charts \
   ghcr.io/helm/chartmuseum:v0.14.0
