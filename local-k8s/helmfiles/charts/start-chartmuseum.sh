9d7944ca1212b7735999fd0432a4e8f1804f843b
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2022-11-10 13:23:39 +0000

    Add chartmuseum docker bootstrap script

---
+++ local-k8s/helmfiles/charts/start-chartmuseum.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+docker run --rm -it \
+  -p 8080:8080 \
+  -e DEBUG=1 \
+  -e STORAGE=local \
+  -e STORAGE_LOCAL_ROOTDIR=/charts \
+  -v $(pwd)/charts:/charts \
+  ghcr.io/helm/chartmuseum:v0.14.0
